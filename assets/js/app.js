b2f010233791410fb0409ac8a041aaadfd2a4086
LegionXIII <42222236+ian-c-nelson@users.noreply.github.com>
2018-10-21 02:25:47 -0700

    Added logic for toggling off diseases. Passed values to map function.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,69 +4,31 @@ var mainController = {
 
     //Inititialization Function - App
     init: function () {
-        console.log("running init function");
-        //build left nav bar content
+        // build left nav bar content
         uiController.buildLeftNavBar(data.countries, data.diseaseGroups);
-        uiController.populateMap(data.countries, data.diseaseGroups);
 
-    },
-
-    //events for button/nav clicks
-
-    // Left half of split button click
-    onDropDownClick: function (event) {
-        var codes = $(this).data("codes");
-
-        console.log("Main Dropdown button clicked (disease group).", event);
-        console.log(codes);
-
-        //call the fetch health data function
-        mainController.fetchHealthData(codes, function () {
-            //  process the data
-
-            // pass the processed data to uiController to populate map
-
-        });
-
-    },
-
-    // drop down menu item click
-    onDropDownItemClick: function (event) {
-        var codes = $(this).data("codes");
-
-        console.log("Dropdown item clicked (disease group).");
-        console.log(event);
-        console.log(codes);
-
-        //call the fetch health data function
-        mainController.fetchHealthData(codes, function () {
-            //  process the data
-
-            // pass the processed data to uiController to populate map
-
-        });
+        // load the data and render the map
+        mainController.loadData();
     },
 
 
     //Functions for retreiving data from WHO 
-    fetchHealthData: function (codes, callback) {
-        // split the codes string so we can pass them as an argument
-        var codesArray = codes.split("|");
-
+    fetchHealthData: function (countries, diseases, callback) {
         //URL for AJAX request
-        var queryUrl = "https://cors-anywhere.herokuapp.com/http://apps.who.int/gho/athena/data/GHO/SDG_SH_DTH_RNCOM?profile=simple&format=json&filter=SEX:BTSX;YEAR:2016;GHECAUSES:" + codesArray[0];
+        var queryUrl = "https://cors-anywhere.herokuapp.com/http://apps.who.int/gho/athena/data/GHO/SDG_SH_DTH_RNCOM?profile=simple&format=json&filter=SEX:BTSX;YEAR:2016";
 
-        if (codesArray[1]=== "*") {
-            data.countries.forEach(function(country){
-               queryUrl+=";COUNTRY:"+country.code; 
-            });
+        diseases.forEach(function (disease) {
+            if (disease.include) {
+                queryUrl += ";GHECAUSES:" + disease.code;
+            }
+        });
 
-        }
-        else {
-            queryUrl+=";COUNTRY:"+codesArray[1]; 
-        }
+        // For now pre-filter for all 5 countries and we will further filter by GHECODE/COUNTRY in the data processing.
+        countries.forEach(function (country) {
+            queryUrl += ";COUNTRY:" + country.code;
+        });
+
         console.log(queryUrl);
-        
 
         //create simplified object and store in data
         $.ajax({
@@ -76,10 +38,91 @@ var mainController = {
             url: queryUrl,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
             callback(response);
         });
     },
+
+    loadData: function () {
+        // toggle the map and spinner
+        uiController.showMap(false);
+        uiController.showSpinner(true);
+
+        mainController.fetchHealthData(data.countries, data.diseaseGroups, function (response) {
+            //  process the data
+            console.log(response);
+
+            // loop through the countries, then the groups and finally the repsonse and add a value to 
+            // the countries object if there is a match
+            data.countries.forEach(function (country) {
+                // clear the values
+                country.values = {};
+                country.weights = {};
+
+                data.diseaseGroups.forEach(function (diseaseGroup) {
+                    if (diseaseGroup.include) {
+                        if (diseaseGroup.includeCountries.indexOf(country.code) !== -1) {
+                            response.fact.forEach(function (item) {
+                                if (diseaseGroup.title.toLowerCase() === item.dim.GHECAUSES.toLowerCase()
+                                    && country.title.toLowerCase() === item.dim.COUNTRY.toLowerCase()) {
+                                    // insert the new values
+                                    var val = parseInt(item.Value);
+                                    country.values[diseaseGroup.code] = val;
+                                    country.weights[diseaseGroup.code] = Math.floor(country.population / val);
+
+                                }
+                            });
+                        }
+                    }
+                });
+            });
+
+            console.log(data.countries);
+
+            // pass the processed data to uiController to populate map
+            uiController.populateMap(data.diseaseGroups, data.countries);
+
+            // toggle the map and spinner in reverse
+            setTimeout(function () {
+                uiController.showSpinner(false);
+                uiController.showMap(true);
+            }, 250);
+        });
+    },
+
+    // Left half of split button click
+    onDropDownClick: function (event) {
+        var dataAttr = $(this).data();
+
+        if (dataAttr.include) {
+            // make sure at least one group stays included
+            var gheCount = $(".btn-group > button > i.fa-check-square").length;
+            if (gheCount <= 1) {
+                event.preventDefault();
+                return;
+            }
+
+            $(this).find("i").toggleClass("fa-square fa-check-square");
+            dataAttr.include = false;
+        } else {
+            $(this).find("i").toggleClass("fa-square fa-check-square");
+            dataAttr.include = true;
+        }
+
+        var codesArray = dataAttr.codes.split("|");
+        $.each(data.diseaseGroups, function () {
+            if (this.code === codesArray[0]) {
+                this.include = dataAttr.include;
+            }
+        });
+
+        // TODO: Figure out how to toggle the countries off and on.
+
+        $(this).data(dataAttr);
+
+        // load the data and render the map
+        mainController.loadData();
+    },
+
 };
 
 
@@ -89,45 +132,10 @@ var uiController = {
     //Add an object to store the selectors
     selectors: {
         googleMap: "#googleMap",
-        leftNav: "#left-nav"
+        leftNav: "#left-nav",
+        spinner: ".spinner"
     },
 
-    //Function for mapping data to Leaflet API
-    populateMap: function (countries) {
-        var zoomLvl = 1.5;
-        // console.log(new google.maps.LatLng(37.782551, -122.445368));
-        var mapProp = {
-            center: new google.maps.LatLng(20, 20),
-            zoom: zoomLvl,
-        };
-        var map = new google.maps.Map($(this.selectors.googleMap)[0], mapProp);
-        var heatmapData = [];
-        for (var country in countries) {
-            var countryObj = countries[country];
-            var marker = new google.maps.Marker({ position: countryObj.coord, map: map, title: countryObj.title });
-            heatmapData.push({
-                location: new google.maps.LatLng(countryObj.coord.lat, countryObj.coord.lng),
-                weight: countryObj.weight
-            });
-        }
-
-        var heatmap = new google.maps.visualization.HeatmapLayer({
-            data: heatmapData,
-            map: map,
-            radius: "300"
-        });
-
-        map.addListener('zoom_changed', function () {
-            // 3 seconds after the center of the map has changed, pan back to the
-            // marker.
-            if (zoomLvl !== map.getZoom()) {
-                zoomLvl = map.getZoom();
-                console.log("Map Zoom Level:", map.getZoom());
-                heatmap.set('radius', zoomLvl * 20);
-                console.log("Heatmap Radius:", heatmap.get('radius'));
-            }
-        });
-    },
 
     //Use the API data to build the left navigation bar 
     buildLeftNavBar: function (countries, diseases) {
@@ -145,10 +153,13 @@ var uiController = {
                     "type": "button",
                     "class": "btn btn-info btn-sm",
                     "data-codes": disease.code + "|*",
+                    "data-include": "true"
                 })
-                .text(disease.title)
+                .text(disease.buttonText)
+                .prepend("<i class=\"fas fa-check-square fa-pull-left\"></i>")
                 .on("click", mainController.onDropDownClick)
                 .appendTo(dropDown);
+
 
 
             //add the right side of the split button
@@ -182,14 +193,84 @@ var uiController = {
                     .attr({
                         class: "dropdown-item",
                         "h-ref": "#",
-                        "data-codes": disease.code + "|" + country.code
+                        "data-codes": disease.code + "|" + country.code,
+                        "data-include": "true"
                     })
                     .text(country.title)
-                    .on("click", mainController.onDropDownItemClick)
+                    .prepend("<i class=\"fas fa-check-square fa-pull-left\"></i>")
+                    .on("click", mainController.onDropDownClick)
                     .appendTo(dropdownContainer);
             });
         });
     },
+
+    //Function for mapping data to Leaflet API
+    populateMap: function (diseaseGroups, countries) {
+        var zoomLvl = 1.5;
+
+        var mapProp = {
+            center: new google.maps.LatLng(20, 20),
+            zoom: zoomLvl,
+        };
+        var map = new google.maps.Map($(this.selectors.googleMap)[0], mapProp);
+
+        var heatMaps = [];
+
+        diseaseGroups.forEach(function (diseaseGroup) {
+            if (diseaseGroup.include) {
+                var heatmapData = [];
+
+                countries.forEach(function (country) {
+                    if (country.values[diseaseGroup.code]) {
+                        var offsetCoord = {
+                            lat: country.coord.lat + diseaseGroup.offset.lat,
+                            lng: country.coord.lng + diseaseGroup.offset.lng
+                        }
+
+                        new google.maps.Marker({
+                            position: offsetCoord,
+                            map: map,
+                            title: diseaseGroup.title + " - " + country.title
+                        });
+
+                        heatmapData.push({
+                            location: new google.maps.LatLng(country.coord.lat + diseaseGroup.offset.lat, country.coord.lng + diseaseGroup.offset.lng),
+                            weight: country.weights[diseaseGroup.code]
+                        });
+                    }
+                });
+
+                var heatmap = new google.maps.visualization.HeatmapLayer({
+                    data: heatmapData,
+                    map: map,
+                    radius: "300",
+                    gradient: diseaseGroup.gradient
+                });
+                heatMaps.push(heatmap);
+            }
+        });
+
+        map.addListener('zoom_changed', function () {
+            // 3 seconds after the center of the map has changed, pan back to the
+            // marker.
+            if (zoomLvl !== map.getZoom()) {
+                zoomLvl = map.getZoom();
+                // console.log("Map Zoom Level:", map.getZoom());
+                heatMaps.forEach(function (heatmap) {
+                    heatmap.set('radius', zoomLvl * 20);
+                    // console.log("Heatmap Radius:", heatmap.get('radius'));
+                });
+            }
+        });
+    },
+
+    showMap: function (show) {
+        $(this.selectors.googleMap).toggleClass("d-none", !show);
+    },
+
+    showSpinner: function (show) {
+        $(this.selectors.spinner).toggleClass("d-none", !show);
+    }
 };
 
 //Build Controller object for data
@@ -202,8 +283,8 @@ var data = {
             coord: { lat: -14.235004, lng: -51.92528 },
             language: "Portuguese",
             title: "Brazil",
-            weight: 80,
-            includeInQuery: true
+            weights: {},
+            values: {}
         },
         {
             code: "CHN",
@@ -211,9 +292,8 @@ var data = {
             coord: { lat: 35.86166, lng: 104.195397 },
             language: "Chinese",
             title: "China",
-            weight: 80,
-            includeInQuery: true
-
+            weights: {},
+            values: {}
         },
         {
             code: "IND",
@@ -221,8 +301,8 @@ var data = {
             coord: { lat: 20.593684, lng: 78.96288 },
             language: "Hindi",
             title: "India",
-            weight: 80,
-            includeInQuery: true
+            weights: {},
+            values: {}
         },
         {
             code: "IDN",
@@ -230,49 +310,121 @@ var data = {
             coord: { lat: -0.789275, lng: 113.921327 },
             language: "Indonesian",
             title: "Indonesia",
-            weight: 80,
-            includeInQuery: true
+            weights: {},
+            values: {}
         },
         {
             code: "USA",
             population: 323995528,
-            coord: { lat: 37.09024, lng: -95.712891 },
+            coord: { lat: 40.09024, lng: -102.712891 },
             language: "English",
-            title: "United States",
-            weight: 80,
-            includeInQuery: true
+            title: "United States of America",
+            weights: {},
+            values: {}
         }
     ],
 
     //Build JSON array of Disease Cause Groups/SubGroups
     diseaseGroups: [
         {
-            title: "Cardiovascular Disease",
+            title: "Cardiovascular Diseases",
+            buttonText: "Cardiovascular Diseases",
             dimension: "GHECAUSES",
             code: "GHE110",
-            value: 0,
-            includeInQuery: true
+            include: true,
+            includeCountries: ["BRA", "CHN", "IND", "IDN", "USA"],
+            offset: { lat: .5, lng: .5 },
+            gradient: [
+                'rgba(0, 255, 255, 0)',
+                'rgba(0, 255, 255, 0.3)',
+                'rgba(0, 191, 255, 0.4)',
+                'rgba(0, 127, 255, 0.5)',
+                'rgba(0, 63, 255, 0.6)',
+                'rgba(0, 0, 255, 0.6)',
+                'rgba(0, 0, 223, 0.7)',
+                'rgba(0, 0, 191, 0.7)',
+                'rgba(0, 0, 159, 0.8)',
+                'rgba(0, 0, 127, 0.8)',
+                'rgba(63, 0, 91, 0.9)',
+                'rgba(127, 0, 63, 0.9)',
+                'rgba(191, 0, 31, 1)',
+                'rgba(255, 0, 0, 1)'
+            ]
         },
         {
-            title: "Diabetes Melitus",
+            title: "Diabetes Mellitus",
+            buttonText: "Diabetes Mellitus",
             dimension: "GHECAUSES",
             code: "GHE080",
-            value: 0,
-            includeInQuery: true
+            include: true,
+            includeCountries: ["BRA", "CHN", "IND", "IDN", "USA"],
+            offset: { lat: -.5, lng: .5 },
+            gradient: [
+                'rgba(0, 255, 255, 0)',
+                'rgba(0, 255, 255, 0.3)',
+                'rgba(0, 191, 255, 0.4)',
+                'rgba(0, 127, 255, 0.5)',
+                'rgba(0, 63, 255, 0.6)',
+                'rgba(0, 0, 255, 0.6)',
+                'rgba(0, 0, 223, 0.7)',
+                'rgba(0, 0, 191, 0.7)',
+                'rgba(0, 0, 159, 0.8)',
+                'rgba(0, 0, 127, 0.8)',
+                'rgba(63, 0, 91, 0.9)',
+                'rgba(127, 0, 63, 0.9)',
+                'rgba(191, 0, 31, 1)',
+                'rgba(255, 0, 0, 1)'
+            ]
         },
         {
             title: "Malignant Neoplasms",
+            buttonText: "Malignant Neoplasms",
             dimension: "GHECAUSES",
             code: "GHE061",
-            value: 0,
-            includeInQuery: true
+            include: true,
+            includeCountries: ["BRA", "CHN", "IND", "IDN", "USA"],
+            offset: { lat: .5, lng: -.5 },
+            gradient: [
+                'rgba(0, 255, 255, 0)',
+                'rgba(0, 255, 255, 0.3)',
+                'rgba(0, 191, 255, 0.4)',
+                'rgba(0, 127, 255, 0.5)',
+                'rgba(0, 63, 255, 0.6)',
+                'rgba(0, 0, 255, 0.6)',
+                'rgba(0, 0, 223, 0.7)',
+                'rgba(0, 0, 191, 0.7)',
+                'rgba(0, 0, 159, 0.8)',
+                'rgba(0, 0, 127, 0.8)',
+                'rgba(63, 0, 91, 0.9)',
+                'rgba(127, 0, 63, 0.9)',
+                'rgba(191, 0, 31, 1)',
+                'rgba(255, 0, 0, 1)'
+            ]
         },
         {
-            title: "Respiratory Diseases",
+            title: "Chronic obstructive pulmonary disease",
+            buttonText: "COPD",
             dimension: "GHECAUSES",
-            code: "GHE117",
-            value: 0,
-            includeInQuery: true
+            code: "GHE118",
+            include: true,
+            includeCountries: ["BRA", "CHN", "IND", "IDN", "USA"],
+            offset: { lat: -.5, lng: -.5 },
+            gradient: [
+                'rgba(0, 255, 255, 0)',
+                'rgba(0, 255, 255, 0.3)',
+                'rgba(0, 191, 255, 0.4)',
+                'rgba(0, 127, 255, 0.5)',
+                'rgba(0, 63, 255, 0.6)',
+                'rgba(0, 0, 255, 0.6)',
+                'rgba(0, 0, 223, 0.7)',
+                'rgba(0, 0, 191, 0.7)',
+                'rgba(0, 0, 159, 0.8)',
+                'rgba(0, 0, 127, 0.8)',
+                'rgba(63, 0, 91, 0.9)',
+                'rgba(127, 0, 63, 0.9)',
+                'rgba(191, 0, 31, 1)',
+                'rgba(255, 0, 0, 1)'
+            ]
         }
     ],
 
